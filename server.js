5a5e34e11289516bf71e58cd1b912823d80190a8
Sarah Lawrence <s.j.lawrence8@gmail.com>
2017-02-25 12:39:36 +1100

    Drops post data on /posts

--- server.js
+++ server.js
@@ -7,16 +7,22 @@ var app = express();
 app.use(express.static('public'));
 app.use(formidable());
 
-fs.readFile(__dirname + '/data/posts.json', function (error, file) {
-  console.log(file.toString());
-
-  var parsedFile = JSON.parse(file);
-});
-
 
 app.post('/create-post', function (req, res) {
   console.log('/create-post was hit');
   console.log(req.fields);
+  
+});
+
+app.get('/posts', function (req, res) {
+  var postData;
+
+  fs.readFile(__dirname + '/data/posts.json', function (error, file) {
+
+    postData = JSON.parse(file);
+    res.send(postData);
+  });
+
 
 });
 
