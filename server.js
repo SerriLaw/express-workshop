325729ef650025091b38c4dab33754469af72054
Sarah Lawrence <s.j.lawrence8@gmail.com>
2017-02-25 12:22:28 +1100

    Added file reading

--- server.js
+++ server.js
@@ -1,14 +1,23 @@
 var express = require('express');
 var formidable = require('express-formidable');
+var fs = require('fs');
 
 var app = express();
 
 app.use(express.static('public'));
 app.use(formidable());
 
+fs.readFile(__dirname + '/data/posts.json', function (error, file) {
+  console.log(file.toString());
+
+  var parsedFile = JSON.parse(file);
+});
+
+
 app.post('/create-post', function (req, res) {
   console.log('/create-post was hit');
   console.log(req.fields);
+
 });
 
 app.listen(3000, function () {
